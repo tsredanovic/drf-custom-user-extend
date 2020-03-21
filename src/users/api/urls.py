94b5cbb9cb813c910601b9ae373688214e4255ca
Toni Sredanović <toni@krakensystems.co>
2020-03-22 00:44:10 +0100

    rest auth basics

---
+++ src/users/api/urls.py
@@ -0,0 +1,13 @@
+from allauth.account.views import confirm_email
+from django.conf.urls import url
+from django.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+urlpatterns = [
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^account/', include('allauth.urls')),
+    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
+        name='account_confirm_email'),
+]
