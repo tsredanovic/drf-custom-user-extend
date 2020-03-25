0ed23b6a73246eb7079c08635aec75f5cdf3a4db
Toni Sredanović <toni@krakensystems.co>
2020-03-25 11:26:27 +0100

    urls cleanup

--- src/users/api/urls.py
+++ src/users/api/urls.py
@@ -1,26 +1,28 @@
-from allauth.account.views import confirm_email
+from allauth.account.views import email_verification_sent
 from django.conf.urls import url
-from django.urls import include
+from django.urls import path
 from rest_framework import routers
 
 from users.api.views import *
 
 router = routers.DefaultRouter()
 urlpatterns = [
-    # Rest auth urls
-    url(r'^password/reset/$', CustomPasswordResetView.as_view(), name='rest_password_reset'),
-    url(r'^password/reset/confirm/$', CustomPasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+    # Login, Logout
     url(r'^login/$', CustomLoginView.as_view(), name='rest_login'),
     url(r'^logout/$', CustomLogoutView.as_view(), name='rest_logout'),
-    url(r'^user/$', CustomUserDetailsView.as_view(), name='rest_user_details'),
-    url(r'^password/change/$', CustomPasswordChangeView.as_view(), name='rest_password_change'),
 
+    # Password
+    url(r'^password/change/$', CustomPasswordChangeView.as_view(), name='rest_password_change'),
+    url(r'^password/reset/$', CustomPasswordResetView.as_view(), name='rest_password_reset'),
+    url(r'^password/reset/confirm/$', CustomPasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+
+    # Registration
     url(r'^registration/$', CustomRegisterView.as_view(), name='rest_register'),
     url(r'^registration/verify-email/$', CustomVerifyEmailView.as_view(), name='rest_verify_email'),
 
-    url(r'^account/', include('allauth.urls')),
-    url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
-        name='account_confirm_email'),
+    # User
+    url(r'^user/$', CustomUserDetailsView.as_view(), name='rest_user_details'),
 
-    url(r'^', include('django.contrib.auth.urls'))
+    # Email
+    path("confirm-email/", email_verification_sent, name="account_email_verification_sent"),  # Needed for registration email
 ]
--- src/users/api/views.py
+++ src/users/api/views.py
@@ -8,11 +8,11 @@ from rest_auth.registration.views import RegisterView, VerifyEmailView
 from rest_auth.serializers import TokenSerializer
 from rest_auth.views import LoginView, PasswordChangeView, PasswordResetView, PasswordResetConfirmView, UserDetailsView
 from rest_framework import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from users.api.serializers import CustomRegisterSerializer, CustomUserDetailsSerializer
+from users.api.serializers import CustomRegisterSerializer
 
 
 class CustomLoginView(LoginView):
